299931908f1c171a82e7f56361d4b12ed7db82e8
GreenAsh <moskalenkoip@gmail.com>
2020-02-14 02:19:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,15 @@
-const swap_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 168v-16c0-13.255 10.745-24 24-24h360V80c0-21.367 25.899-32.042 40.971-16.971l80 80c9.372 9.373 9.372 24.569 0 33.941l-80 80C409.956 271.982 384 261.456 384 240v-48H24c-13.255 0-24-10.745-24-24zm488 152H128v-48c0-21.314-25.862-32.08-40.971-16.971l-80 80c-9.372 9.373-9.372 24.569 0 33.941l80 80C102.057 463.997 128 453.437 128 432v-48h360c13.255 0 24-10.745 24-24v-16c0-13.255-10.745-24-24-24z"/></svg>`;
+miro.onReady(() => {
+  const icon24 = '<g id="Layer_1"><path d="M2.796,2.468 L2.749,22.468" fill-opacity="0" stroke="#000000" stroke-width="2"/><path d="M3.5,21.5 L23.564,21.5" fill-opacity="0" stroke="#000000" stroke-width="2"/><path d="M2.796,14.783 C3.209,13.972 3.594,13.171 4.059,12.371 C4.623,11.401 5.385,10.556 5.953,9.606 C6.402,8.857 7.01,8.19 7.426,7.395 C7.433,7.382 7.577,7.143 7.584,7.143 C7.752,7.143 8.14,9.507 8.163,9.606 C8.417,10.699 9.623,17.334 10.268,17.95 C10.54,18.21 10.69,17.331 10.846,16.995 C11.092,16.467 11.363,15.957 11.636,15.437 C12.745,13.317 13.466,11.153 14.687,9.053 C15.97,6.848 15.997,5.125 18.107,8.148 C18.173,8.244 19.421,9.809 19.475,9.757 C20.284,8.984 20.992,7.211 21.527,6.188" fill-opacity="0" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g>'
 
-
-const EMPTY_STATE = {
-    swapStarted: false,
-    widget: false,
-    unlockId: false
-}
-window.state = {
-    swapStarted: false,
-    widget: false,
-    unlockId: false
-}
-
-miro.onReady(async () => {
-    const authorized = await miro.isAuthorized();
-    if (!authorized) {
-        return;
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'magic pallete',
+        svgIcon: icon24,
+        onClick: () => {
+          miro.board.ui.openLeftSidebar('sidebar.html')
+        }
+      },
     }
-
-    await miro.initialize({
-      
-        extensionPoints: {
-            getWidgetMenuItems: async (widgets) => {
-                if (!widgets || widgets.length !== 1) {
-                    return [];
-                }
-                const widget = widgets[0];
-                return [{
-                    tooltip: 'Swap',
-                    svgIcon: swap_icon,
-                    onClick: async (widgets) => {
-                        if (window.state.swapStarted === false) {
-                            window.state = await startSwap(widget);
-                            await miro.board.selection.clear();
-                        } else if (widget.id !== window.state.widget.id) {
-                            swapWith([widget.id], widget.bounds);
-                        }
-                    }
-                }]
-            }
-        }
-    })
-    
-    miro.addListener('SELECTION_UPDATED', async (e) => {
-        if (window.state.swapStarted === false) {
-            return;
-        }
-        
-        const widgets = e.data;
-        if (!widgets || widgets.length === 0) {
-            return;
-        }
-        
-        for (let i = 0; i < widgets.length; i++) {
-            if (widgets[i].id === window.state.widget.id) {
-                console.log("Can't swap with starting widget")
-                return;
-            }
-        }
-        
-        const bounds = await miro.board.figma.getWidgetsBounds(widgets);
-        await swapWith(widgets.map(({id}) => id), bounds);
-    })
-});
-
-async function swapWith(widgetIds, targetBounds){
-    const state = window.state
-    const sourceBounds = state.widget.bounds;
-    
-    const dx = sourceBounds.x - targetBounds.x;
-    const dy = sourceBounds.y - targetBounds.y;
-    
-    await miro.board.widgets.transformDelta(state.widget.id, dx, dy);
-    await miro.board.widgets.transformDelta(widgetIds, -dx, -dy);
-    stopSwap(state)
-}
-
-async function startSwap(widget){
-    if (!widget || (widget.capabilities && widget.capabilities.editable === false)) {
-        return EMPTY_STATE;
-    }
-    await miro.board.widgets.update({"id": widget.id, capabilities: {editable: false}})
-    const timeoutId = setTimeout(async () => {
-        await stopSwap(window.state) 
-    }, 20000);
-    
-    return {
-        swapStarted: true,
-        widget: widget,
-        unlockId: timeoutId
-    };
-}
-
-async function stopSwap(state) {
-    if (state.widget && state.widget.capabilities && state.widget.capabilities.editable === false) {
-        await miro.board.widgets.update({"id": widget.id, capabilities: {editable: true}})
-    }
-    clearInterval(state.unlockId);
-    window.state = EMPTY_STATE;
-}
+  })
+})
