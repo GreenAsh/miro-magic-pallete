ada233d16e0382981753da820a52c68c30a603ea
GreenAsh <moskalenkoip@gmail.com>
2020-02-14 13:33:26 +0500

    Update palletes.js

--- palletes.js
+++ palletes.js
@@ -1,6 +1,6 @@
 window.getPalletes = function() {
     return [{
-        name: "Test Pallete",
+        name: "https://coolors.co/003049-d62828-f77f00-fcbf49-eae2b7",
         colors: ["#003049", "#D62828", "#F77F00", "#FCBF49", "#EAE2B7" ]
     }];
 }
